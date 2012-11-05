8c29ea4fbc64c7a0100aff2ecfc7a5fc6dd3b043
JeremyZoss <jzoss@swri.org>
2012-11-05 19:15:59 +0000

    Fix compile issues (MotoPlus, industrial_robot_client)

--- simple_message/src/message_manager.cpp
+++ simple_message/src/message_manager.cpp
@@ -166,6 +166,19 @@ void MessageManager::spinOnce()
   }
 }
 
+int ms_per_clock;
+void mySleep(int sec)
+{
+#ifdef MOTOPLUS
+  if (ms_per_clock <= 0)
+    ms_per_clock = mpGetRtc();
+
+  mpTaskDelay(sec * 1000 / ms_per_clock);
+#else
+  sleep(sec);
+#endif
+}
+
 void MessageManager::spin()
 {
   LOG_INFO("Entering message manager spin loop");
@@ -179,7 +192,7 @@ void MessageManager::spin()
 
     // Throttle loop speed if waiting for a re-connection
     if (!this->getConnection()->isConnected())
-      sleep(5);
+      mySleep(5);
   }
 }
 
