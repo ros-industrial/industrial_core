a1c9b6a7f98c10d5057e76dec9fd3f58ee6e4aca
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2014-01-21 15:16:50 +0100

    robot_client: workaround for #46. Fix #67.
    This is an updated version of the workaround committed in 9df46977. Instead
    of requiring dependent packages to invoke the function defined in the
    CFG_EXTRAS cmake snippet, the snippet now sets up the linker path directly.
    
    Dependent packages now only need to remember to explicitly list their
    dependency on `industrial_robot_client` and `simple_message` in their
    `add_library(..)` statements.

--- simple_message/cmake/simple_message_linker_setup.cmake
+++
@@ -1,9 +0,0 @@
-
-function(simple_message_linker_setup)
-    if (DEFINED simple_message_LIBRARY_DIRS)
-    else()
-        message(FATAL_ERROR "simple_message_LIBRARY_DIRS not set, "
-            "have you find_package()-ed it?")
-    endif()
-    link_directories(${simple_message_LIBRARY_DIRS})
-endfunction()
