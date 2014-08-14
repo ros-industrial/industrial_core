cd8ffb258b9bee0ecdb3a80f1333bad091acbe35
thiagodefreitas <thiagodefreitas@gmail.com>
2014-08-14 08:05:04 +0200

    Small changes and clean

--- industrial_robot_client/src/joint_trajectory_action2.cpp
+++ industrial_robot_client/src/joint_trajectory_action2.cpp
@@ -1,3 +1,36 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2014, Fraunhofer IPA
+ * Author: Thiago de Freitas
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 	* Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.
+ * 	* Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * 	* Neither the name of the Fraunhofer IPA, nor the names
+ *	of its contributors may be used to endorse or promote products derived
+ *	from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <industrial_robot_client/joint_trajectory_action2.h>
 #include <industrial_robot_client/utils.h>
 #include <industrial_utils/param_utils.h>
@@ -22,10 +55,11 @@ JointTrajectoryAction2::JointTrajectoryAction2() :
 
   std::map<int,RobotGroup> robot_groups;
 
+  //This loops through the yaml file to find the parameters for correctly configuring the node
+  //A parameter named topics_list contains the respective information for each of the groups
+
   std::string value;
   ros::param::search("topics_list", value);
-
-  ROS_INFO("%s", value.c_str());
 
   XmlRpc::XmlRpcValue topics_list_rpc;
   ros::param::get(value,topics_list_rpc);
