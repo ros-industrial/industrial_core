3646ceb93aea716188b8d676e5b183530eb4de9d
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2021-06-21 15:46:44 +0200

    Formatting

--- simple_message/cmake/platform_build_flags.cmake
+++ simple_message/cmake/platform_build_flags.cmake
@@ -2,16 +2,16 @@
 # defaults set here will be exported to dependent projects via catkin. Dependent projects can use
 # cmake's `remove_definitions` command to change these defaults if desired.
 
-#Include code that interfaces with ROS
+# Include code that interfaces with ROS
 add_definitions(-DSIMPLE_MESSAGE_USE_ROS)
 
-#Control which platform's underlying networking library is used by this package.
-#The package will not build unless one (and only one) of these definitions is set.
+# Control which platform's underlying networking library is used by this package.
+# The package will not build unless one (and only one) of these definitions is set.
 add_definitions(-DSIMPLE_MESSAGE_LINUX)
 #add_definitions(-DSIMPLE_MESSAGE_MOTOPLUS)
 
 #--------------------------------------------------------------------------
-#Old definitions (deprecated in kinetic, to be removed in lunar or melodic)
+# Old definitions (deprecated in kinetic, to be removed in lunar or melodic)
 #--------------------------------------------------------------------------
 #add_definitions(-DROS)
 #add_definitions(-DLINUXSOCKETS)
