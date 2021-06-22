27f8314b437bd44dd383d8fd07c84517a782e9a9
G.A. vd. Hoorn <g.a.vanderhoorn@tudelft.nl>
2021-06-22 21:08:41 +0200

    Rebased 238: TcpClient won't re-establish connection (#263)
    Squashed commits:
    
    * Issue-238 Fix tested  on indigo & Fanuc
    * Issue-238 Cleanup that didn't get pushed
    * Issue-238 Add copyright for mods
    
    Co-authored-by: 98aa713aa8355e307a05fba5721b6ed31d764bb9 <paul.glaubitz@ready-robotics.com>

--- simple_message/include/simple_message/socket/tcp_client.h
+++ simple_message/include/simple_message/socket/tcp_client.h
@@ -2,6 +2,7 @@
  * Software License Agreement (BSD License)
  *
  * Copyright (c) 2011, Southwest Research Institute
+ * Copyright (c) 2019, READY Robotics Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -75,6 +76,9 @@ public:
 
     // Overrides
     bool makeConnect();
+
+private:
+    bool connectSocketHandle();
 
 
 };
